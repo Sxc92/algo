f9a5631a44aecfcad56ee7d6ac01e1c1c1251811
sxc <xiecheng.shi@raipiot.com>
2023-09-18 12:31:27 +0800

    数组实现

---
+++ linearity/src/main/java/com/chris/array/Case.java
@@ -0,0 +1,25 @@
+package com.chris.array;
+
+/**
+ * Question :
+ *
+ * @author 史偕成
+ * @date 2023/09/17 22:52
+ **/
+public class Case {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Question : https://leetcode.com/problems/move-zeroes/description/
+     * 将数组0移动到最后
+     *
+     * @param nums
+     */
+    public static void moveZero(int[] nums) {
+        int ids = 0;
+
+    }
+}
